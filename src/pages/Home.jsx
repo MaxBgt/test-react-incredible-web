895132bad5c73f231d42aabac4db59cfe5dad13e
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-03-17 07:37:38 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+
+export default function Home() {
+  const state = useSelector((state) => state);
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
